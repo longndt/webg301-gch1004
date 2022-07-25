3a31ae401aa0bf248974b26f37b467ac785dee1f
longndt <longndt226@gmail.com>
2022-07-25 11:58:41 +0700

    form

--- form1/src/Form/NoteType.php
+++ form1/src/Form/NoteType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Note;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,27 @@ class NoteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
-            ->add('date')
-            ->add('image')
+            ->add('content', TextType::class,
+            [
+                'required' => true,  //nullable = false
+                'label' => 'Nội dung',
+                'attr' => 
+                [
+                    'minlength' => 5,
+                    'maxlength' => 20
+                ]
+            ])
+            ->add('image', TextType::class,
+            [
+                'required' => true,
+                'label' => 'Hình ảnh',
+            ])
+            ->add('date', DateType::class,
+            [
+                'label' => 'Deadline',
+                'required' => true,
+                'widget' => 'single_text'
+            ])
         ;
     }
 
