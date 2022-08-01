c97a1cf7d31799898e3d48cf33f6d7254133c610
longndt <longndt226@gmail.com>
2022-08-01 11:12:47 +0700

    bookstore

--- bookstore/src/Controller/AuthorController.php
+++ bookstore/src/Controller/AuthorController.php
@@ -2,12 +2,68 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/author')]
 class AuthorController extends AbstractController
 {
-     
+   #[Route('/index', name: 'author_index')]
+   public function authorIndex () {
+      $authors = $this->getDoctrine()->getRepository(Author::class)->findAll();
+      return $this->render('author/index.html.twig',
+        [
+            'authors' => $authors
+        ]);
+   } 
+
+   #[Route('/list', name: 'author_list')]
+   public function authorList() {
+      $authors = $this->getDoctrine()->getRepository(Author::class)->findAll();
+      return $this->render('author/list.html.twig',
+        [
+            'authors' => $authors
+        ]);
+   } 
+
+   #[Route('/detail/{id}', name: 'author_detail')]
+   public function authorDetail ($id, AuthorRepository $authorRepository) {
+      $author = $authorRepository->find($id);
+      if ($author == null) {
+         $this->addFlash('Warning','Invalid author id !');
+         return $this->redirectToRoute('author_index');
+      }
+      return $this->render('author/detail.html.twig',
+        [
+            'author' => $author
+        ]);   
+   }
+
+   #[Route('/delete/{id}', name: 'author_delete')]
+   public function authorDelete ($id, ManagerRegistry $managerRegistry) {
+     $author = $managerRegistry->getRepository(Author::class)->find($id);
+     if ($author == null) {
+        $this->addFlash('Warning', 'Author not existed !');
+     } else {
+        $manager = $managerRegistry->getManager();
+        $manager->remove($author);
+        $manager->flush();
+        $this->addFlash('Info', 'Delete author succeed !');
+     }
+     return $this->redirectToRoute('author_index');
+   }
+
+   #[Route('/add', name: 'author_add')]
+   public function authorAdd (Request $request) {
+
+   }
+
+   #[Route('/edit/{id}', name: 'author_edit')]
+   public function authorEdit ($id, Request $request) {
+
+   }
 }
--- bookstore/src/Controller/GenreController.php
+++ bookstore/src/Controller/GenreController.php
@@ -2,20 +2,68 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Genre;
+use App\Repository\GenreRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/genre')]
 class GenreController extends AbstractController
 {
   #[Route('/index', name: 'genre_index')]
-  public function genreIndex () {
+   public function genreIndex () {
+      $genres = $this->getDoctrine()->getRepository(Genre::class)->findAll();
+      return $this->render('genre/index.html.twig',
+        [
+            'genres' => $genres
+        ]);
+   } 
 
-  }
+   #[Route('/list', name: 'genre_list')]
+   public function genreList() {
+      $genres = $this->getDoctrine()->getRepository(Genre::class)->findAll();
+      return $this->render('genre/list.html.twig',
+        [
+            'genres' => $genres
+        ]);
+   } 
 
-  #[Route('/detail/{id}', name: 'genre_detail' )]
-  public function genreDetail ($id) {
-    
-  }
+   #[Route('/detail/{id}', name: 'genre_detail')]
+   public function genreDetail ($id, GenreRepository $genreRepository) {
+      $genre = $genreRepository->find($id);
+      if ($genre == null) {
+         $this->addFlash('Warning','Invalid genre id !');
+         return $this->redirectToRoute('genre_index');
+      }
+      return $this->render('genre/detail.html.twig',
+        [
+            'genre' => $genre
+        ]);   
+   }
+
+   #[Route('/delete/{id}', name: 'genre_delete')]
+   public function genreDelete ($id, ManagerRegistry $managerRegistry) {
+     $genre = $managerRegistry->getRepository(Genre::class)->find($id);
+     if ($genre == null) {
+        $this->addFlash('Warning', 'Genre not existed !');
+     } else {
+        $manager = $managerRegistry->getManager();
+        $manager->remove($genre);
+        $manager->flush();
+        $this->addFlash('Info', 'Delete genre succeed !');
+     }
+     return $this->redirectToRoute('genre_index');
+   }
+
+   #[Route('/add', name: 'genre_add')]
+   public function genreAdd (Request $request) {
+
+   }
+
+   #[Route('/edit/{id}', name: 'genre_edit')]
+   public function genreEdit ($id, Request $request) {
+
+   }
 }
